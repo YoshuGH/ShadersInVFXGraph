320d74b09cdf552bca17eb554a52d801051d66e3
YoshuGH <axeleonardo2000@gmail.com>
2021-05-21 20:26:39 -0500

    All Proyect

---
+++ ShadersTest/Assets/ShadersTest/Scripts/DissolveTarget.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[ExecuteInEditMode]
+public class DissolveTarget : MonoBehaviour
+{
+    public Transform objToTrack = null;
+
+    private Material matRef = null;
+    private new Renderer renderer = null;
+
+    public Renderer Renderer
+    {
+        get
+        {
+            if(renderer == null)
+            {
+                renderer = this.GetComponent<Renderer>();
+            }
+
+            return renderer;
+        }
+    }
+
+    public Material MaterialRef
+    {
+        get
+        {
+            if (matRef == null)
+            {
+                matRef = Renderer.material;
+            }
+
+            return matRef;
+        }
+    }
+
+    private void Awake()
+    {
+        renderer = this.GetComponent<Renderer>();
+        matRef = renderer.material;
+    }
+
+    private void Update()
+    {
+        if(objToTrack != null)
+        {
+            MaterialRef.SetVector("_pos", objToTrack.position);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        renderer = null;
+        if (matRef != null)
+            Destroy(matRef);
+
+        matRef = null;
+    }
+}
---
+++ ShadersTest/Assets/ShadersTest/Scripts/MovingTarget.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTarget : MonoBehaviour
+{
+    public float speed;
+    public int dir;
+    [SerializeField] private float dist;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(dir,0,0) * speed * Time.deltaTime, Space.World);
+
+        dist = Vector3.Distance(new Vector3(dir * 2, 1.641f, 1), transform.position);
+
+        if (dist <= 0.1)
+        {
+            dir = -dir;
+            dist = 0;
+        }
+    }
+}
